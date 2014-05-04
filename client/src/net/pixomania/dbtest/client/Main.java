59831a63a132818aa4e7107c99a35c494b52d988
Victor Nagy <galaxyAbstractor@gmail.com>
2014-05-04 20:49:15 +0200

    Improved client

--- client/src/net/pixomania/dbtest/client/Main.java
+++ client/src/net/pixomania/dbtest/client/Main.java
@@ -11,35 +11,53 @@ import java.util.LinkedList;
 
 public class Main {
 	public static int testing_database = 1; // 1 = mysql, 2 = db4o, 3 = hibernate
-	public static String url = "http://localhost:9000";
+	public static String url = "http://192.168.1.3:9000";
 
 	public static void main(String[] args) {
 
 		try {
-			LinkedList<Airport> airports =	Util.loadAirports(new File("C:\\Users\\Victor\\dbtest\\client\\data\\airports.csv"), 3376);
+			//LinkedList<Airport> airports =	Util.loadAirports(new File("C:\\Users\\Victor\\dbtest\\client\\data\\airports.csv"), 3376);
 			ConnectionPool.open(5);
-			for (int x = 0; x < 5; x++) {
-				for (int i = 1; i < 4; i++) {
-					testing_database = i;
+			for(int dataamount = 0; dataamount < 3; dataamount++) {
+				for (int repetitions = 0; repetitions < 3; repetitions++) {
+					for (int database = 1; database < 4; database++) {
+						testing_database = database;
+	/*
+						InsertAirportTest insertAirportTest = new InsertAirportTest();
+						insertAirportTest.run(airports);
+						System.out.println("[" + new Date() + "] Insert airports test done, run: " + (repetitions + 1) + ", database: " + testing_database);
+	*/
+						InsertFlightTest insertFlightTest = new InsertFlightTest();
+						int toload = 0;
 
-					InsertAirportTest insertAirportTest = new InsertAirportTest();
-					insertAirportTest.run(airports);
-					System.out.println("[" + new Date() + "] Insert airports test done, run: " + (x + 1) + ", database: " + testing_database);
+						switch (dataamount) {
+							case 0:
+								toload = 1000;
+								break;
+							case 1:
+								toload = 10000;
+								break;
+							case 2:
+								toload = 20000;
+								break;
+							case 3:
+								toload = 80000;
+								break;
+						}
+						LinkedList<Flight> flights = Util.loadFlights(new File("1987.csv"), toload);
+						insertFlightTest.run(flights);
+						System.out.println("[" + new Date() + "] Insert flights test done, run: " + (repetitions + 1) + ", database: " + testing_database);
 
-					InsertFlightTest insertFlightTest = new InsertFlightTest();
-					LinkedList<Flight> flights = Util.loadFlights(new File("C:\\Users\\Victor\\dbtest\\client\\data\\1987.csv"), 3000);
-					insertFlightTest.run(flights);
-					System.out.println("[" + new Date() + "] Insert flights test done, run: " + (x + 1) + ", database: " + testing_database);
-
-					SelectFlightTest selectFlightTest = new SelectFlightTest();
-					selectFlightTest.run(flights);
-					System.out.println("[" + new Date() + "] Select flights test done, run: " + (x + 1) + ", database: " + testing_database);
-
-					ListFlightTest listFlightTest = new ListFlightTest();
-					listFlightTest.run(airports);
-					System.out.println("[" + new Date() + "] List fligths test done, run: " + (x + 1) + ", database: " + testing_database);
-
-					HttpClient.sendGET(url + "/purge");
+						SelectFlightTest selectFlightTest = new SelectFlightTest();
+						selectFlightTest.run(flights);
+						System.out.println("[" + new Date() + "] Select flights test done, run: " + (repetitions + 1) + ", database: " + testing_database);
+	/*
+						ListFlightTest listFlightTest = new ListFlightTest();
+						listFlightTest.run(airports);
+						System.out.println("[" + new Date() + "] List fligths test done, run: " + (repetitions + 1) + ", database: " + testing_database);
+	*/
+						HttpClient.sendGET(url + "/purge");
+					}
 				}
 			}
 
