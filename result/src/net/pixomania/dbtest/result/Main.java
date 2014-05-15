99fb1520df6848902759e3a487ca39c13b28d92e
Victor Nagy <galaxyAbstractor@gmail.com>
2014-05-15 15:52:14 +0200

    Added result module

--- result/src/net/pixomania/dbtest/result/Main.java
+++ result/src/net/pixomania/dbtest/result/Main.java
@@ -5,7 +5,12 @@ import net.pixomania.dbtest.result.database.Database;
 public class Main {
 
 	public static void main(String[] args) {
-		Database db = new Database();
-		System.out.println(db.calculateRequestTotalAverage(65, 71, 77));
+		Database db = new Database(20000);
+		int i = 232;
+		System.out.println(db.calculateRequestTotal(i) + "\t" + db.calculateRequestTotal(i + 6) + "\t" + db.calculateRequestTotal(i + 12));
+		i += 2;
+		System.out.println(db.calculateRequestTotal(i) + "\t" + db.calculateRequestTotal(i + 6) + "\t" + db.calculateRequestTotal(i + 12));
+		i += 2;
+		System.out.println(db.calculateRequestTotal(i) + "\t" + db.calculateRequestTotal(i + 6) + "\t" + db.calculateRequestTotal(i + 12));
 	}
 }
